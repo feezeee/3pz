81f7f2a24b10c32cec22ba2a850ab6b3ac3a5915
Денис Скурат <den.skur2016@yandex.ru>
2021-10-09 15:12:28 +0300

    Начал адаптивность 768px

--- assets/js/timeTransfer.js
+++ assets/js/timeTransfer.js
@@ -2,43 +2,57 @@
                 let date = new Date();
                 
                 let time = date.getHours();
-                let _dateTransfer = document.getElementById('dateTransfer');
-                let dateTransferUsers = new Date(_dateTransfer.value);
+                let _dateTransfer = document.getElementById('dateTransfer'); 
+                let dateTransferUsers = new Date(_dateTransfer.value);// Получаем выбранную дату
                 
               if(dateTransferUsers.getMonth() > date.getMonth() || dateTransferUsers.getDate() > date.getDate())
               {
                 // ничего не удаляем
-                let option = document.getElementById('1');
-                    option.parentNode.appendChild(option);
-              }
+                let optionList = document.getElementById('timeTransfer').getElementsByTagName('option');
+
+                  for (i = 0; i < optionList.length; i++) {                      
+                      
+                    optionList[i].disabled = false;                      
+                  }
                 
-              
-              
-              
-              
-              if ( 9 <= time )
+                    
+              }
+              else {
+                
+                if ( 9 <= time )
                 {
                     let option = document.getElementById('1');
-                    option.parentNode.removeChild(option);
+                    option.disabled = true;
+                    option = document.getElementById('2');                    
+                    option.parentNode.value = option;
                 }
                 if ( 12 <= time )
                 {
                     let option = document.getElementById('2');
-                    option.parentNode.removeChild(option);
+                    option.disabled = true;
+                    option = document.getElementById('3');                    
+                    option.parentNode.value = option;
+                                     
+                  option.parentNode.disabled = false;
                 }
                 if ( 15 <= time )
                 {
-                    let option = document.getElementById('3');
-                      option.parentNode.removeChild(option);
+                  let option = document.getElementById('3');
+                  option.disabled = true;    
+                  option = document.getElementById('4');                    
+                  option.parentNode.value = option;                
                 }
                 if ( 18 <= time )
                 {
                   let option = document.getElementById('4');
-                      option.parentNode.removeChild(option);
+                  option.disabled = true;
+                  option = document.getElementById('5');                    
+                  option.parentNode.disabled = false;
                 }
                 if ( 21 <= time )
                 {
                   let option = document.getElementById('5');
-                      option.parentNode.removeChild(option);
+                  option.disabled = true;          
                 }
-              };    
+              }              
+        };    
